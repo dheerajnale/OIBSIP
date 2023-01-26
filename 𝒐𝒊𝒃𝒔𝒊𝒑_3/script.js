efd00e80e6903f1d4726551bc9ad14ae6c9e73f1
Nale Dheeraj <110556177+dheerajnale@users.noreply.github.com>
2023-01-26 23:26:03 +0530

    Add files via upload

---
+++ 𝒐𝒊𝒃𝒔𝒊𝒑_3/script.js
@@ -0,0 +1,27 @@
+/*-----------------javascript file----------------------*/
+const calculateTemp = () => {
+    const inputTemp = document.getElementById('temp').value;
+
+    const tempSelected = document.getElementById('temp_diff');
+    const valueTemp = temp_diff.options[tempSelected.selectedIndex].value;
+
+// Celsius to Fahrenheit Conversion
+    const celToFah = (cel) => {
+        let fahrenheit = ((cel * 9 / 5) + 32).toFixed(1);
+        return fahrenheit;
+    }
+
+// Fahrenheit to Celsius Conversion
+    const fahToCel = (fah) => {
+        let celsius = ((fah - 32) * 5 / 9).toFixed(1);
+        return celsius;
+    }
+
+//output of the temperature conversion
+    if (valueTemp == 'cel') {
+        document.getElementById("result").innerHTML = celToFah(inputTemp) + "&#176; Fahrenheit";
+    }
+    else {
+        document.getElementById("result").innerHTML = fahToCel(inputTemp) + "&#176; Celsius";
+    }
+}
